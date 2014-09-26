af6b58c9909b12f7a0c4bdcdf42045b480a6d7f8
Jari Vetoniemi <mailroxas@gmail.com>
2014-09-26 20:26:24 +0300

    Use --workdir in git commands.

--- client/protocols/git.py
+++ client/protocols/git.py
@@ -24,16 +24,16 @@ def clone(srcdir, url, branch, result):
     if os.path.isdir(os.path.join(srcdir, '.git')):
         os.chdir(srcdir)
         oldcommit = _git2('rev-parse', 'HEAD').strip().decode('UTF-8')
-        if not _git('fetch', '-f', '-u', 'origin', '{}:{}'.format(branch, branch)):
+        if not _git('--work-tree', srcdir, 'fetch', '-f', '-u', 'origin', '{}:{}'.format(branch, branch)):
             raise DownloadException('git fetch failed')
-        if not _git('checkout', '-f', branch):
+        if not _git('--work-tree', srcdir, 'checkout', '-f', branch):
             raise DownloadException('git checkout failed')
     elif not _git('clone', '--depth', '1', '-b', branch, url, srcdir):
         raise DownloadException('git clone failed')
 
     os.chdir(srcdir)
-    result['commit'] = _git2('rev-parse', 'HEAD').strip().decode('UTF-8')
-    result['description'] = _git2('log', '-1', '--pretty=%B').strip().decode('UTF-8')
+    result['commit'] = _git2('--work-tree', srcdir, 'rev-parse', 'HEAD').strip().decode('UTF-8')
+    result['description'] = _git2('--work-tree', srcdir, 'log', '-1', '--pretty=%B').strip().decode('UTF-8')
 
     if os.path.exists(os.path.join(srcdir, '.buildhck_built')) and result['commit'] == oldcommit:
         raise NothingToDoException('There is nothing to build')
