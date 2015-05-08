e279975f7248905432b2b0e079c1389ef4855a67
Zack Buhman <zack@buhman.org>
2015-05-08 12:11:55 -0400

    setup: add test and lint

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+class PyTest(test):
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = []
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+
+class PyLint(PyTest):
+    def initialize_options(self):
+        super().initialize_options()
+        self.pytest_args = ['--pylint', '--ignore=tests']
 
 setup(
     name='buildhck',
@@ -8,9 +28,14 @@ setup(
     maintainer='Buildhck Team',
     include_package_data=True,
     packages=find_packages(),
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'buildhckc = buildhck.client.client:main'
         ]
+    },
+    tests_require=['pytest'],
+    cmdclass={
+        'test': PyTest,
+        'lint': PyLint
     }
 )
