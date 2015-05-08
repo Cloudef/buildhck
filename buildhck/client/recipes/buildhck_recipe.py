7fbd7b2fb744beb7e749593dabfe2255688bbf5a
Jari Vetoniemi <mailroxas@gmail.com>
2015-05-08 18:38:14 +0300

    buildhck.client: Use tox -c instead of build_in_srcdir

--- buildhck/client/recipes/buildhck_recipe.py
+++ buildhck/client/recipes/buildhck_recipe.py
@@ -6,12 +6,9 @@ upstream = 'https://github.com/Cloudef/buildhck'
 source = 'git+git://github.com/Cloudef/buildhck.git#branch=master'
 build = ['python3 -m py_compile "$srcdir/buildhck/buildhck.py"',
          'python3 -m py_compile "$srcdir/buildhck/client/client.py"']
-test = ['tox']
+test = ['tox -c $srcdir/tox.ini']
 analyze = ['pylint "$srcdir/buildhck/buildhck.py" "$srcdir/buildhck/client/client.py"']
 package = ['python3 $srcdir/setup.py install --root="$pkgdir/"']
 analyze_re = '^W:'
-
-# tox needs tox.ini in $PWD
-build_in_srcdir = True
 
 # vim: set ts=8 sw=3 tw=0
