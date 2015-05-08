76f4edb2877aa899467d1a13f305ca7ec0ff21b9
Jari Vetoniemi <mailroxas@gmail.com>
2015-05-08 18:15:30 +0300

    buildhck.client: Update buildhck recipe

--- buildhck/client/recipes/buildhck_recipe.py
+++ buildhck/client/recipes/buildhck_recipe.py
@@ -4,10 +4,14 @@
 name = 'buildhck'
 upstream = 'https://github.com/Cloudef/buildhck'
 source = 'git+git://github.com/Cloudef/buildhck.git#branch=master'
-build = ['python3 -m py_compile "$srcdir/buildhck.py"',
-         'python3 -m py_compile "$srcdir/client/client.py"']
-test = ['python3 "$srcdir/test.py"']
-analyze = ['pylint "$srcdir/buildhck.py" "$srcdir/client/client.py"']
+build = ['python3 -m py_compile "$srcdir/buildhck/buildhck.py"',
+         'python3 -m py_compile "$srcdir/buildhck/client/client.py"']
+test = ['tox']
+analyze = ['pylint "$srcdir/buildhck/buildhck.py" "$srcdir/buildhck/client/client.py"']
+package = ['python3 $srcdir/setup.py install --root="$pkgdir/"']
 analyze_re = '^W:'
+
+# tox needs tox.ini in $PWD
+build_in_srcdir = True
 
 # vim: set ts=8 sw=3 tw=0
